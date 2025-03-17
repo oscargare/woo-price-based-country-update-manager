29fd1e4c243598eb559013a6cfdea358c3118cd0
Oscar Gare <ogarcia.arenas@gmail.com>
2025-03-17 16:13:45 +0100

    fix: Plugin object: Set defaults properties on instance

--- includes/plugins/class-plugin.php
+++ includes/plugins/class-plugin.php
@@ -42,12 +42,23 @@ abstract class Plugin {
 	 * @return void
 	 */
 	public function __construct() {
-		$this->set_version();
-		$this->set_name();
-		$this->set_slug();
-		$this->set_property( 'new_version', $this->get_version() );
-		$this->set_property( 'tested', '' );
-		$this->set_property( 'package', '' );
+		$this->set_defaults();
+	}
+
+	/**
+	 * Sets defaults.
+	 */
+	protected function set_defaults() {
+		$plugin_data = get_plugin_data( $this->get_file(), false, false );
+
+		$this->data = [
+			'version'     => isset( $plugin_data['Version'] ) ? $plugin_data['Version'] : '',
+			'name'        => plugin_basename( $this->get_file() ),
+			'slug'        => plugin_basename( basename( $this->get_file(), '.php' ) ),
+			'new_version' => isset( $plugin_data['Version'] ) ? $plugin_data['Version'] : '',
+			'tested'      => '',
+			'package'     => false,
+		];
 	}
 
 	/**
@@ -73,15 +84,6 @@ abstract class Plugin {
 	}
 
 	/**
-	 * Sets version from plugin file
-	 */
-	protected function set_version() {
-		$data    = get_plugin_data( $this->get_file(), false, false );
-		$version = isset( $data['Version'] ) ? $data['Version'] : false;
-		$this->set_property( 'version', $version );
-	}
-
-	/**
 	 * Returns version.
 	 *
 	 * @return string
@@ -91,26 +93,12 @@ abstract class Plugin {
 	}
 
 	/**
-	 * Sets name from plugin file.
-	 */
-	protected function set_name() {
-		$this->set_property( 'name', plugin_basename( $this->get_file() ) );
-	}
-
-	/**
 	 * Returns name.
 	 *
 	 * @return string
 	 */
 	public function get_name() {
 		return $this->get_property( 'name' );
-	}
-
-	/**
-	 * Sets slug from plugin file.
-	 */
-	protected function set_slug() {
-		$this->set_property( 'name', plugin_basename( basename( $this->get_file(), '.php' ) ) );
 	}
 
 	/**
@@ -156,11 +144,11 @@ abstract class Plugin {
 
 		$data = $this->update_check_request();
 
-		if ( is_wp_error( $data ) || ! is_object( $data ) || ! isset( $data->version, $data->tested, $data->package ) ) {
+		if ( is_wp_error( $data ) || ! is_object( $data ) || ! isset( $data->new_version, $data->tested, $data->package ) ) {
 			return;
 		}
 
-		$this->set_property( 'new_version', $data->version );
+		$this->set_property( 'new_version', $data->new_version );
 		$this->set_property( 'tested', $data->tested );
 		$this->set_property( 'package', $data->package );
 	}
